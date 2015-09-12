cf9ac228dcf6cc35825f775dbdea487863929f67
Jessey Eagan <jesseyeagan@Jesseys-MacBook-Air.local>
2015-09-12 11:09:06 -0600

    end of the dayday

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ angular.module('iDecidedThis', []);
 // Setting Controller
 angular.module('iDecidedThis').controller("mainController", ["$scope", function($scope){ 
 	$scope.greeting = "Hello people!"
+	$scope.unorderedList = "List to come..."
 	$scope.buttonClick = function() {
 		console.log("I should have asked for help earlier!")
 		$scope.greeting = "WOOHOO!!!!!!!"
+	
+	
+	$scope.buttonClick2 = function() {
+		console.log("Will I ever get the hang of this?")
+		$scope.unorderedList = [ 
+	 		'Jessica',
+	 		'Eagan',
+	 		'Jessey'
+	 ]
 	}
-
+}
 
 
 
